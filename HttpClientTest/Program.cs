1a5cddd05c3652723e5d61eb0c60f44354f32eb3
vanva <vanva@CUCMBR>
2023-10-11 22:15:20 +0300

    Добавьте файлы проекта.

--- HttpClientTest/Program.cs
+++ HttpClientTest/Program.cs
@@ -5,28 +5,21 @@ namespace BonchMark
 {
     class Program
     {
-        private static HttpClient client = new HttpClient()
+        private static readonly HttpClient client = new HttpClient()
         {
             BaseAddress = new Uri("https://lk.sut.ru/cabinet/?login=yes")
         };
 
-        static async Task PostAsync(HttpClient client)
-        {
-            using StringContent jsonContent = new(
-                JsonSerializer.Serialize(new
-                {
-                    userId = 77,
-                    id = 1,
-                    title = "write code sample",
-                    completed = false
-                }),
-                Encoding.UTF8,
-                "application/json");
-        }
-
+        // we have to make working and convinient way of interacting with data we get from client.
+        // тут точно замешано асинхронное программирование, насколько я понял TAP. Ещё в целом надо понять, все ли геты нужно повторять.
+        // вот так
+        //
         static async Task Main()
         {
-
+            StringContent content = new StringContent("BENRO"); // бенро?? крутяк))
+            using var response = await client.PostAsync(client.BaseAddress, content);
+            string responseText = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseText);
         }
 
     }
