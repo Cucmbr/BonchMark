12a7ca7b91f9cafe4faebc9fdd316617f582ae57
vanva <vanva@CUCMBR>
2023-10-11 22:15:20 +0300

    Добавьте файлы проекта.

---
+++ HttpClientTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using System.Text;
+
+namespace BonchMark
+{
+    class Program
+    {
+        private static HttpClient client = new HttpClient()
+        {
+            BaseAddress = new Uri("https://lk.sut.ru/cabinet/?login=yes")
+        };
+
+        static async Task PostAsync(HttpClient client)
+        {
+            using StringContent jsonContent = new(
+                JsonSerializer.Serialize(new
+                {
+                    userId = 77,
+                    id = 1,
+                    title = "write code sample",
+                    completed = false
+                }),
+                Encoding.UTF8,
+                "application/json");
+        }
+
+        static async Task Main()
+        {
+
+        }
+
+    }
+}
