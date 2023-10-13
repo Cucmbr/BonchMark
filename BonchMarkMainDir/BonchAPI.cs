ccb0f44253f7dc236e087d9e9f9efa3e03bb3fc0
иван шиткин <vanvanich531@gmail.com>
2023-10-13 11:39:11 +0300

    WorkingLogin + WorkingTimetable

--- BonchMarkMainDir/BonchAPI.cs
+++ BonchMarkMainDir/BonchAPI.cs
@@ -2,14 +2,11 @@
 {
     internal class BonchAPI
     {
-        private readonly HttpClient _httpClient;
-        public HttpClient HttpClient 
-        { 
-            get => _httpClient;
-        }
+        public HttpClient HttpClient { get; }
+
         public BonchAPI()
         {
-            _httpClient = new(new HttpClientHandler() { AutomaticDecompression = System.Net.DecompressionMethods.None })
+            HttpClient = new(new HttpClientHandler() { AutomaticDecompression = System.Net.DecompressionMethods.GZip }, true)
             {
                 BaseAddress = new Uri("https://lk.sut.ru")
             };
@@ -29,20 +26,38 @@
             //client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.967 YaBrowser/23.9.1.967 Yowser/2.5 Safari/537.36");
             //client.DefaultRequestHeaders.Add("X-Requested-With", "XMLHttpRequest");
         }
+
         public async Task Test()
         {
-            using StringContent content = new StringContent("users=tsolomatin1704%40gmail.com&parole=Pidor228");
-            using var response = await HttpClient.PostAsync(_httpClient.BaseAddress + "/cabinet/lib/autentificationok.php", content);
-            string responseText = await response.Content.ReadAsStringAsync();
-            Console.WriteLine("Login:\n" + responseText);
-            Console.WriteLine(response.StatusCode);
+            HttpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.967 YaBrowser/23.9.1.967 Yowser/2.5 Safari/537.36");
+
+            using var getResponse = await HttpClient.GetAsync("https://lk.sut.ru/");
+            string getResponseText = await getResponse.Content.ReadAsStringAsync();
+            //Console.WriteLine(getResponseText);
+
+            using StringContent loginRequest = new StringContent("users=tsolomatin1704%40gmail.com&parole=Pidor228", null, "application/x-www-form-urlencoded");
+            using var loginResponse = await HttpClient.PostAsync(HttpClient.BaseAddress + "/cabinet/lib/autentificationok.php", loginRequest);
+            string loginResponseText = await loginResponse.Content.ReadAsStringAsync();
+            Console.WriteLine("Login:\n" + loginResponseText);
+            Console.WriteLine(loginResponse.StatusCode);
             Console.WriteLine("--------------------------------------------------------------------");
 
-            using StringContent content2 = new StringContent("key=6118");
-            using var response2 = await HttpClient.PostAsync(this.HttpClient.BaseAddress + "/cabinet/project/cabinet/forms/raspisanie.php", content2);
-            Console.WriteLine(response2.StatusCode);
-            string responseText2 = await response2.Content.ReadAsStringAsync();
-            Console.WriteLine(responseText2);
+            using var getResponse2 = await HttpClient.GetAsync("https://lk.sut.ru/cabinet/?login=yes");
+            string getResponseText2 = await getResponse2.Content.ReadAsStringAsync();
+            //Console.WriteLine(getResponseText2);
+
+            using StringContent timetableRequest = new StringContent("key=6118", null, "application/x-www-form-urlencoded");
+            using var timetableResponse = await HttpClient.PostAsync(HttpClient.BaseAddress + "/cabinet/project/cabinet/forms/raspisanie.php", timetableRequest);
+            string responseText = await timetableResponse.Content.ReadAsStringAsync();
+            Console.WriteLine("Timetable:\n" + responseText);
+            Console.WriteLine(timetableResponse.StatusCode);
+            Console.WriteLine("--------------------------------------------------------------------");
+
+            using StringContent markRequest = new StringContent("Вставить запрос на отметку!");
+            using var markResponse = await HttpClient.PostAsync(HttpClient.BaseAddress + "", markRequest);
+            string markResponseText = await markResponse.Content.ReadAsStringAsync();
+            //Console.WriteLine("Mark:\n" + markResponseText);
+            Console.WriteLine(markResponse.StatusCode);
             Console.WriteLine("--------------------------------------------------------------------");
         }
     }
--- BonchMarkMainDir/Program.cs
+++ BonchMarkMainDir/Program.cs
@@ -5,7 +5,7 @@
         static async Task Main(string[] args)
         {
             BonchAPI api = new BonchAPI();
-            api.Test();
+            await api.Test();
         }
     }
 }
