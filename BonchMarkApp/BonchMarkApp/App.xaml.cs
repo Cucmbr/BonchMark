fe0ce48afc62435ee6efae92ce274cf5d4f812a0
TApocEK111 <realms1337@rambler.ru>
2023-10-14 18:03:19 +0300

    Login page interface v1.0

--- BonchMarkApp/BonchMarkApp/App.xaml.cs
+++ BonchMarkApp/BonchMarkApp/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace BonchMarkApp
 {
@@ -9,7 +7,7 @@ namespace BonchMarkApp
         public App()
         {
             InitializeComponent();
-            MainPage = new LoginPage();
+            MainPage = new MainPage(); //MainPage LoginPage
         }
 
         protected override void OnStart()
