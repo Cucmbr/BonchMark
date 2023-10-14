15f4e47cdf9fa4bee8899dcc5c97042bda2286cc
TApocEK111 <realms1337@rambler.ru>
2023-10-14 10:15:54 +0300

    NewStructure v1.1, optimizations, edge cases

--- BonchMarkMainDir/BonchAPI.cs
+++ BonchMarkMainDir/BonchAPI.cs
@@ -3,24 +3,30 @@ using System.Net;
 
 namespace BonchMark
 {
-    internal class BonchAPI
+    public class BonchAPI
     {
         private HttpClient _httpClient;
-        public string Ivan { get { return "users=vanvanich531%40gmail.com&parole=2MFPBNG8RHB"; } }
-        private string Tima { get { return "users=tsolomatin1704%40gmail.com&parole=Pidor228"; } }
+
+        private string[] _ivan = { "vanvanich531%40gmail.com", "2MFPBNG8RHB" }; //For testing ONLY. It's not a final solution!
+        private string[] _tima = { "tsolomatin1704%40gmail.com", "Pidor228" };
+        private class LkRequest : StringContent
+        {
+            public LkRequest(string content)
+                : base(content, null, "application/x-www-form-urlencoded") { }
+        }
+
         private enum MarkState
         {
             NoButton,
             UpdateOnly,
             IncorrectOpenZan,
-            requestFailed,
+            RequestFailed,
             OK
-
         }
 
         public BonchAPI()
         {
-            _httpClient = new(new HttpClientHandler() { AutomaticDecompression = System.Net.DecompressionMethods.GZip }, true)
+            _httpClient = new(new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip }, true)
             {
                 BaseAddress = new Uri("https://lk.sut.ru")
             };
@@ -38,9 +44,9 @@ namespace BonchMark
         }
         private async Task<bool> Login(string users, string parole)
         {
-            using StringContent loginRequest = new StringContent($"users={users}&parole={parole}", null, "application/x-www-form-urlencoded");
+            using LkRequest loginRequest = new LkRequest($"users={users}&parole={parole}");
             using var loginResponse = await _httpClient.PostAsync(_httpClient.BaseAddress + "/cabinet/lib/autentificationok.php", loginRequest);
-            if (loginResponse.StatusCode != HttpStatusCode.OK)
+            if (InvalidResponseCheck(loginResponse))
             {
                 return false;
             }
@@ -51,20 +57,14 @@ namespace BonchMark
         }
         private async Task<string> PullTimetable()
         {
-            using StringContent timetableRequest = new StringContent("key=6118", null, "application/x-www-form-urlencoded");
+            using LkRequest timetableRequest = new LkRequest("key=6118");
             using var timetableResponse = await _httpClient.PostAsync(_httpClient.BaseAddress + "/cabinet/project/cabinet/forms/raspisanie.php", timetableRequest);
-            if (timetableResponse.StatusCode != HttpStatusCode.OK)
+            if (InvalidResponseCheck(timetableResponse))
             {
                 return "";
             }
             string text = await timetableResponse.Content.ReadAsStringAsync();
             return text;
-        }
-        private HtmlDocument CreateHtmlDoc(string content)
-        {
-            HtmlDocument htmlDoc = new HtmlDocument();
-            htmlDoc.LoadHtml(content);
-            return htmlDoc;
         }
         private async Task<MarkState> Mark()
         {
@@ -79,11 +79,11 @@ namespace BonchMark
                 {
                     return MarkState.IncorrectOpenZan;
                 }
-                using StringContent markRequest = new StringContent($"open=1&rasp={openZanArr[0]}&week=={openZanArr[1]}", null, "application/x-www-form-urlencoded");
+                using LkRequest markRequest = new LkRequest($"open=1&rasp={openZanArr[0]}&week=={openZanArr[1]}");
                 using var markResponse = await _httpClient.PostAsync(_httpClient.BaseAddress + "/cabinet/project/cabinet/forms/raspisanie.php", markRequest);
-                if (markResponse.StatusCode != HttpStatusCode.OK)
+                if (InvalidResponseCheck(markResponse))
                 {
-                    return MarkState.requestFailed;
+                    return MarkState.RequestFailed;
                 }
                 string markResponseText = await markResponse.Content.ReadAsStringAsync();
                 if (markResponseText.Contains("id:0"))
@@ -96,45 +96,25 @@ namespace BonchMark
             else
                 return MarkState.NoButton;
         }
-        public async Task Test()
+        private HtmlDocument CreateHtmlDoc(string content)
         {
-            //_httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.967 YaBrowser/23.9.1.967 Yowser/2.5 Safari/537.36");
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(content);
+            return htmlDoc;
+        }
+        private bool InvalidResponseCheck(HttpResponseMessage message)
+        {
+            if (message == null && message.StatusCode != HttpStatusCode.OK)
+                return true;
+            else
+                return false;
+        }
+        public async Task MarkSequence() //For testing ONLY. It's not a final solution!
+        {
+            await Init();
+            await Login(_tima[0], _tima[1]);
+            await Mark();
 
-            //using var getResponse = await _httpClient.GetAsync("https://lk.sut.ru/");
-            //string getResponseText = await getResponse.Content.ReadAsStringAsync();
-            ////Console.WriteLine(getResponseText);
-
-            //using StringContent loginRequest = new StringContent(_tima, null, "application/x-www-form-urlencoded");
-            //using var loginResponse = await _httpClient.PostAsync(_httpClient.BaseAddress + "/cabinet/lib/autentificationok.php", loginRequest);
-            //string loginResponseText = await loginResponse.Content.ReadAsStringAsync();
-            //Console.WriteLine("Login:\n" + loginResponseText);
-            //Console.WriteLine(loginResponse.StatusCode);
-            //Console.WriteLine("--------------------------------------------------------------------");
-
-            //using StringContent timetableRequest = new StringContent("key=6118", null, "application/x-www-form-urlencoded");
-            //using var timetableResponse = await _httpClient.PostAsync(_httpClient.BaseAddress + "/cabinet/project/cabinet/forms/raspisanie.php", timetableRequest);
-            //string responseText = await timetableResponse.Content.ReadAsStringAsync();
-            ////Console.WriteLine("Timetable:\n" + responseText);
-            //Console.WriteLine(timetableResponse.StatusCode);
-            //Console.WriteLine("--------------------------------------------------------------------");
-
-            //var doc = new HtmlDocument();
-            //doc.LoadHtml(responseText);
-            //var doc = CreateHtmlDoc(await PullTimetable());
-            //string? openZan = doc.DocumentNode.SelectSingleNode("/div[@class='container-fluid']/table[@class='simple-little-table']/tbody/tr/td/span/a").OuterHtml;
-            //openZan = openZan.Substring(21, 8);
-            //string[] openZanArr = openZan.Split(',');
-            //if (openZanArr.Length != 2)
-            //{
-            //    Console.WriteLine("Benro");
-            //    return;
-            //}
-            //using StringContent markRequest = new StringContent($"open=1&rasp={openZanArr[0]}&week=={openZanArr[1]}", null, "application/x-www-form-urlencoded");
-            //using var markResponse = await _httpClient.PostAsync(_httpClient.BaseAddress + "/cabinet/project/cabinet/forms/raspisanie.php", markRequest);
-            //string markResponseText = await markResponse.Content.ReadAsStringAsync();
-            //Console.WriteLine("Mark:\n" + markResponseText);
-            //Console.WriteLine(markResponse.StatusCode);
-            //Console.WriteLine("--------------------------------------------------------------------");
         }
     }
 }
