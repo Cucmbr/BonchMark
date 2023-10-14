dbea07025beba143aa838e37eaecf4930e62a341
Cucmbr <vanvanich531@gmail.com>
2023-10-14 20:05:19 +0300

    Starting logic done v1, login logic begin

--- BonchMarkApp/BonchMarkApp/App.xaml.cs
+++ BonchMarkApp/BonchMarkApp/App.xaml.cs
@@ -1,17 +1,33 @@
 ﻿using Xamarin.Forms;
+using System.IO;
+using System;
+using ;
 
 namespace BonchMarkApp
 {
     public partial class App : Application
     {
+        static internal string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        static internal string[] files;
+        static internal BonchMark.
+
         public App()
         {
             InitializeComponent();
-            MainPage = new MainPage(); //MainPage LoginPage
         }
 
         protected override void OnStart()
         {
+            files = Directory.GetFiles(folderPath);
+            if(files.Length != 1 || File.ReadAllText(files[0]).Length == 0)
+            {
+                MainPage = new LoginPage();
+            }
+            else
+            {
+                MainPage = new MainPage();
+            }
+
         }
 
         protected override void OnSleep()
--- BonchMarkApp/BonchMarkApp/LoginPage.xaml.cs
+++ BonchMarkApp/BonchMarkApp/LoginPage.xaml.cs
@@ -1,14 +1,24 @@
-﻿using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
 
 namespace BonchMarkApp
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void OnButtonLogin(object sender, EventArgs args)
+        {
+            if()
+            if (App.files.Length != 1)
+            {
+                File.WriteAllText(Path.Combine(App.folderPath, "UserData.txt"), users)
+            }
         }
     }
 }
