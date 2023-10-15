7a0b57cb0dd866d367a3c7f55f0d76f15d80b9ff
Cucmbr <vanvanich531@gmail.com>
2023-10-15 16:45:07 +0300

    Release v1.0.1

--- BonchMarkApp/BonchMarkApp/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("FiraSans-Bold.ttf", Alias = "Fira Sans Bold")]
-[assembly: ExportFont("FiraSans-Regular.ttf", Alias = "Fira Sans Reg")]
