b233c0b508cce0c3346a4595fd8fabcd9a6d4bfb
Cucmbr <vanvanich531@gmail.com>
2023-10-22 16:54:43 +0300

    Beta developer edition release

--- BonchMarkBlazor/App.xaml.cs
+++ BonchMarkBlazor/App.xaml.cs
@@ -1,11 +1,19 @@
-﻿namespace BonchMarkBlazor
+﻿using BonchMark;
+
+namespace BonchMarkBlazor
 {
     public partial class App : Application
     {
+        static internal string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        static internal string[] files;
+        static internal BonchAPI api = new BonchAPI();
+
         public App()
         {
             InitializeComponent();
 
+            files = Directory.GetFiles(folderPath, "*.txt");
+            
             MainPage = new MainPage();
         }
     }
