3b76a20c7e240eb86493eab5a69db1e4e723129b
Cucmbr <vanvanich531@gmail.com>
2023-10-27 18:59:44 +0300

    Trying to lock login screen

--- BonchMarkBlazor/AppShell.xaml.cs
+++ BonchMarkBlazor/AppShell.xaml.cs
@@ -2,8 +2,10 @@ namespace BonchMarkBlazor;
 
 public partial class AppShell : Shell
 {
+	public static TabBar tabBar;
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+		tabBar = ShellTabBar;
+    }
 }
